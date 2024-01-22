eb09676d2bea8865c966574350238ef10afef394
コマリン親衛隊 <masato190411@icloud.com>
2024-01-22 09:37:53 +0900

    Update main.rs

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use poise::serenity_prelude as serenity;
+mod utils
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let token = std::env::var("DISCORD_TOKEN")?;
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await?;
+    client.start().await?;
+    Ok(())
 }
